4ce87e0027a257d0a82115a23c580a9dd8c20b0f
yingyi mao <yingyi_mao@cmu.ac.th>
2024-06-28 13:40:08 +0700

    3.6

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ createApp({
     setup(){
         const product = ref('Boots')
         const description = ref('abc')
+        const image = ref('./assets/images/socks_green.jpg')
+        const goToCmu = () =>{window.location.href = 'https://www.camt.cmu.ac.th'}
         return{
             product,
-            description
+            description,
+            image,
+            goToCmu
         }
         
       
