97931dd857195564b5a6a2877bb737101f32250d
yingyi mao <yingyi_mao@cmu.ac.th>
2024-06-28 15:37:35 +0700

    5.5

--- main.js
+++ main.js
@@ -7,13 +7,30 @@ createApp({
         const inStock = ref(true)
         const inventory = ref(100)
 
-        const onSale = ref(false)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            { id:2234,color:'green' },
+            { id:2235,color:'blue'}
+        ])
+        const sizes = ref([
+            'S',
+            'M',
+            'L'
+        ])
+
+        
         return{
             product,
             image,
             inStock,
             inventory,
-            onSale
+            details,
+            variants,
+            sizes
         }
     }
 
