6bde405d6b47cca76435fbe19383178b0cc0e70b
yingyi mao <yingyi_mao@cmu.ac.th>
2024-06-29 21:43:36 +0700

    8.5

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const{ createApp,ref } = Vue
+const{ createApp,ref,computed } = Vue
 
 createApp({
     setup(){
         const product = ref('Boots')
-        const image = ref('./assets/images/socks_green.jpg')
-        const inStock = ref(false)
+        const brand = ref('SE 331')
         const inventory = ref(100)
+        const onSale = ref(true)
 
         const details = ref([
             '50% cotton',
@@ -13,21 +13,40 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            { id:2234,color:'green', image: './assets/images/socks_green.jpg'},
-            { id:2235,color:'blue', image:'./assets/images/socks_blue.jpg'}
+            { id:2234,color:'green', image: './assets/images/socks_green.jpg', quantity:50},
+            { id:2235,color:'blue', image:'./assets/images/socks_blue.jpg', quantity:0}
         ])
+        const selectedVariant = ref(0)
+
         const cart = ref(0)
+
+        const title = computed(() =>{
+            if(onSale){
+            return brand.value+' '+product.value+' is on sale!!!!!!!!!!!!'}
+        })
+
+        const image = computed(() =>{
+            return variants.value[selectedVariant.value].image
+        })
+        const inStock = computed(() =>{
+            return variants.value[selectedVariant.value].quantity
+        })
+
+
         function addToCart(){
             cart.value +=1
         }
 
-        function updateImage(variantImage){
-            image.value = variantImage
+        
+
+        function updateVariant(index){
+            selectedVariant.value = index;
+
         }
         
       
         return{
-            product,
+            title,
             image,
             inStock,
             inventory,
@@ -35,7 +54,8 @@ createApp({
             variants,
             cart,
             addToCart,
-            updateImage,
+            updateVariant,
+
             
         }
     }
